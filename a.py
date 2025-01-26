7107bd17985cf125c0984a3b52b4fd485efd4075
steve12512 <86954812+steve12512@users.noreply.github.com>
2025-01-26 18:45:55 +0200

    Analyzed more datasets and found bias in a korean income dataset

---
+++ a.py
@@ -0,0 +1,7 @@
+
+
+def eteria(name,number,licence):
+    adia =  'I have a licence' if licence else  'I dont have a licence'
+    print(f'My name is {name}, I ve beeb working at  ergo for {number} years  and {adia}')
+
+eteria('george',10,True)

8f625b396854ab2e103c5f44ef401ff044628b1a
steve12512 <86954812+steve12512@users.noreply.github.com>
2025-03-02 21:42:11 +0200

    added lots

--- a.py
+++
@@ -1,7 +0,0 @@
-
-
-def eteria(name,number,licence):
-    adia =  'I have a licence' if licence else  'I dont have a licence'
-    print(f'My name is {name}, I ve beeb working at  ergo for {number} years  and {adia}')
-
-eteria('george',10,True)
